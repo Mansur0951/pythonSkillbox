19be21743af45e0d28fe0789cdac130aef070414
Mans <someemail@gmail.com>
2024-07-14 13:34:21 +0300

    finished my 6th HW

--- 6.7 Практическая работа/8.py
+++ 6.7 Практическая работа/8.py
@@ -1,18 +1,21 @@
 guess_number = int(input("Введите число: "))
-left_side = 50
+left_side = 1
 right_side = 100
 att = 1
 metka = 50
 
 while True:
-    comp_gues = int(input(f"Твоё число равно, меньше или больше, чем число {metka}? "))
+    print(f"Твоё число равно, меньше или больше, чем число {metka}?")
+    comp_gues = int(input(f"1 — равно, 2 — больше, 3 — меньше "))
     if comp_gues == 1:
         print("I WON")
         break
-    elif comp_gues == 2:
-        metka = (metka - 101) // 2
-    elif comp_gues == 3:
-        metka = (metka + 101) // 2
+    elif comp_gues == 2: #more
+        left_side = metka
+        metka = (metka + right_side) // 2
+    elif comp_gues == 3: #less
+        right_side = metka
+        metka = (left_side + right_side) // 2
 
     att += 1
 
