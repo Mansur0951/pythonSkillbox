c1f55b753938a5fcf1785372749081ef7ad842c2
Mans <m.arsamikov@zmeke.com>
2024-07-17 18:15:07 +0300

    need to switch the branch

--- 6.7 Практическая работа /1.py
+++ 6.7 Практическая работа /1.py
@@ -1,1 +1,9 @@
-print("some code")
+balance = int(input("How much you received? "))
+
+while balance > 5000:
+    prod_cost = int(input("Put price: "))
+    if (balance < prod_cost) > 0:
+        continue
+    balance -= prod_cost
+
+print("You dont have money (")
